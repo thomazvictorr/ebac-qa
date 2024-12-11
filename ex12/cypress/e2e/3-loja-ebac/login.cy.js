6b1c0a430af3b78fe9a0f897e7bc04cd8f2bad61
Thomaz Victor <thomazvictorr@gmail.com>
2024-12-11 17:57:00 -0300

    Melhorias indentação de código

--- ex12/cypress/e2e/3-loja-ebac/login.cy.js
+++ ex12/cypress/e2e/3-loja-ebac/login.cy.js
@@ -41,18 +41,12 @@ describe('Funcionalidade: Login', () =>{
             cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, thomazvictorr (não é thomazvictorr? Sair)')
         });
 
-        it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        it('Deve fazer login com sucesso - Usando Fixture', () => {
             cy.fixture('perfil').then( dados => {
             cy.get('#username').type(dados.usuario , {log: false})
             cy.get('#password').type(dados.senha , {log: false})
             cy.get('.woocommerce-form > .button').click()
             cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, thomazvictorr (não é thomazvictorr? Sair)')
-
-
-
-
-
-
             })
         }); 
 
