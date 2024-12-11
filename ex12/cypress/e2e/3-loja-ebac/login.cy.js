be17729cb6ed4aada92c397edd05f8c04ec91d9d
Thomaz Victor <thomazvictorr@gmail.com>
2024-12-11 17:54:27 -0300

    Melhorias no código

--- ex12/cypress/e2e/3-loja-ebac/login.cy.js
+++ ex12/cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+const perfil = require ('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () =>{
     beforeEach(() => {
@@ -32,5 +33,29 @@ describe('Funcionalidade: Login', () =>{
             cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail thomazvictorr@gmail.com está incorreta.')
             cy.get('.woocommerce-error').should('exist')
         });
-    
+
+        it('Deve fazer login com sucesso - Usando massa de dados', () => {
+            cy.get('#username').type(perfil.usuario)
+            cy.get('#password').type(perfil.senha)
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, thomazvictorr (não é thomazvictorr? Sair)')
+        });
+
+        it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+            cy.fixture('perfil').then( dados => {
+            cy.get('#username').type(dados.usuario , {log: false})
+            cy.get('#password').type(dados.senha , {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, thomazvictorr (não é thomazvictorr? Sair)')
+
+
+
+
+
+
+            })
+        }); 
+
+
+
     })
