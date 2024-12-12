362e4b5472062458bf469298e5cbcd52cbd11437
Thomaz Victor <thomazvictorr@gmail.com>
2024-12-12 00:21:29 -0300

    Massa de dados em lista

--- ex12/cypress/e2e/3-loja-ebac/produtos.cy.js
+++ ex12/cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -11,17 +11,32 @@ describe('Funcionalidade: Produtos', () => {
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Celeste Sports Br'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
     it('Deve visitar a página do produto', () => {
-
+        produtosPage.visitarProduto('Celeste Sports Br')
+        cy.get('.product_title').should('contain', 'Celeste')
     });
 
     it('Deve adicionar o produto ao carrinho', () => {
-        
+        let qtd = 4
+        produtosPage.buscarProduto('Miko Pullover Hoodie')
+        produtosPage.addProdutoCarrinho('L', 'Orange', qtd)
+        cy.get('.woocommerce-message').should('contain',  qtd  + ' × “Miko Pullover Hoodie” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar o produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[2].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[2].tamanho,
+                dados[2].cor,
+                dados[2].tamanho)
+            cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+        })
     });
 });
--- ex12/cypress/support/page-objects/produtos.page.js
+++ ex12/cypress/support/page-objects/produtos.page.js
@@ -14,12 +14,17 @@ class ProdutosPage{
         .click()
     }
 
-    visitarProduto(){
-        // codigo
+    visitarProduto(nomeProduto){
+        // cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
 
-    addProdutoCarrinho(){
-        // codigo
+    addProdutoCarrinho(tamanho, cor, quantidade){
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
     }
 }
 
