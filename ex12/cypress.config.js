ff03aae8203dddf9430aa2b63ee9e2dc0aa42721
Thomaz Victor <thomazvictorr@gmail.com>
2024-12-13 19:53:15 -0300

    Adicionado comando p/ gravação

--- ex12/cypress.config.js
+++ ex12/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
