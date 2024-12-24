ec3f0cc1c42b23f4d0b51fb0311b0f45fbce5415
Thomaz Victor <thomazvictorr@gmail.com>
2024-12-24 00:32:38 -0300

    Comandos

--- ex12/cypress/support/commands/commands.js
+++ ex12/cypress/support/commands/commands.js
@@ -1,6 +1,8 @@
+/// <reference types="cypress" />
+
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha)
+    cy.get('#password').type(senha, {log: false})
     cy.get('.woocommerce-form > .button').click()
 })
 
@@ -20,3 +22,21 @@ Cypress.Commands.add('detalhesConta', (nome, sobrenome, usuario) => {
     cy.get('#account_display_name').clear().type(usuario)
     cy.get('.woocommerce-Button').click()
 })
+
+Cypress.Commands.add('completeCheckout', () => {
+    cy.fixture('checkoutData').then(dados => {
+      cy.visit('checkout')
+    
+      cy.get('#billing_first_name').clear().type(dados.nome)
+      cy.get('#billing_last_name').clear().type(dados.sobrenome)
+      cy.get('#billing_company').clear().type(dados.empresa)
+      cy.get('#select2-billing_country-container').click().type(dados.país + '{enter}')
+      cy.get('#billing_address_1').clear().type(dados.endereco)
+      cy.get('#billing_address_2').clear().type(dados.complemento)
+      cy.get('#billing_city').clear().type(dados.cidade)
+      cy.get('#select2-billing_state-container').click().type(dados.estado + '{enter}')
+      cy.get('#billing_postcode').clear().type(dados.cep)
+      cy.get('#billing_phone').clear().type(dados.telefone)
+      cy.get('#billing_email').clear().type(dados.email)
+    })
+});
