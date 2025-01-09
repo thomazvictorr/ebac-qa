40dd06ad100940765c2e1d216a3b8ae4f29b5a09
thomazvictorr <thomazvictorr@gmail.com>
2025-01-08 23:39:33 -0300

    Comandos

--- ex14/teste-api-cypress/cypress/support/commands.js
+++ ex14/teste-api-cypress/cypress/support/commands.js
@@ -51,3 +51,17 @@ Cypress.Commands.add('cadastrarProduto', (token, produto, preco, descricao, quan
         failOnStatusCode: false
     })
 })
+Cypress.Commands.add('cadastrarUsuario', (nome, email, senha, adm) => {
+    cy.request({
+        method: 'POST',
+        url: 'usuarios',
+        headers: {authorization: true},
+        body: {
+            "nome": nome,
+            "email": email,
+            "password": senha,
+            "administrador": adm
+          },
+          failOnStatusCode: false
+    })
+})
